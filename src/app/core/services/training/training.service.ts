cbe48c6a10cc61e5038b71a68b1f4723c44ec7d8
Stiven Agudelo <oficialsac@gmail.com>
2023-07-26 07:05:26 -0500

    Training service works

--- src/app/core/services/training/training.service.ts
+++ src/app/core/services/training/training.service.ts
@@ -1,20 +1,21 @@
 /* eslint-disable */
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Form } from '@angular/forms';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
-  baseUrl: string = 'http://127.0.0.1:8000'
+  baseUrl: string = 'http://127.0.0.1:8080/api/data'
 
   headers = new HttpHeaders()
   constructor(private http: HttpClient) { 
 
   }
 
-  sendPost(body: any): Observable<any> {
-    return this.http.post(`${this.baseUrl}/training`, body,{responseType: 'text'});
+  sendPost(body: FormData): Observable<string> {
+    return this.http.post(`${this.baseUrl}/training`, body ,{responseType: 'text'});
   }
 }
