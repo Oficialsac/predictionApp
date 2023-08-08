6427fb7c8f24c0ea0098d0cf9b4bec62e1d2a8e5
Stiven Agudelo <oficialsac@gmail.com>
2023-08-08 16:55:23 -0500

    change style to back button

--- src/app/shared/components/back-button/back-button.component.ts
+++ src/app/shared/components/back-button/back-button.component.ts
@@ -3,7 +3,7 @@ import { Location } from '@angular/common';
 @Component({
   selector: 'app-back-button',
   templateUrl: './back-button.component.html',
-  styleUrls: ['./back-button.component.scss']
+  styleUrls: ['./back-button.component.css']
 })
 export class BackButtonComponent {
 
